1336d04bb5b3b3c4451ccea179680aca33ecfd4a
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2023-10-11 12:08:33 +0200

    all

---
+++ 04.10/Form1.cs
@@ -0,0 +1,54 @@
+using System.Security.Policy;
+
+namespace _04._10
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btsStart_Click(object sender, EventArgs e)
+		{
+			long intTemp = (long)nudNumberN.Value;
+			MessageBox.Show(intTemp + " number of Fibonacci sequence is: " + fib2(intTemp));
+		}
+
+		long fib(long n)
+		{
+			if (n == 0)
+				return 0;
+			else if (n == 1)
+				return 1;
+			else
+				return fib(n - 1) + fib(n - 2);
+		}
+
+		
+         long fib2(long n)
+        {
+			if(n == 0)
+			{
+				return 0;
+			}
+			if(n == 1)
+			{
+				return 1;
+			}
+
+            long[] a = new long[n + 1];
+            a[1] = 1;
+
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                a[i] = a[i - 1] + a[i - 2];
+
+            }
+            return a[n];
+
+        }
+    }
+}
